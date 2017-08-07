072a641bb12f95a5b9926998898c865a414f78ef
Preethi Kasireddy <iam.preethi.k@gmail.com>
2017-08-07 10:38:33 -0700

    Add GMT Safe contract

---
+++ contracts/Safe/GMTSafe.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.14;
+
+import './Tokens/StandardToken.sol';
+
+// Need to deposit X number of GMT here
+contract GMTSafe {
+  mapping (address => uint256) allocations;
+  uint256 public unlockDate;
+  address public GMTAdress;
+  uint256 public constant decimals = 18;
+
+  function GMTSafe(address _GMTAdress) {
+    GMTAdress = _GMTAdress;
+    unlockDate = now + 6 * 30 days;
+
+    // TODO: Add allocations
+    allocations[0] = 0;
+  }
+
+  function unlock() external {
+    assert(now >= unlockDate);
+
+    uint256 entitled = allocations[msg.sender];
+    allocations[msg.sender] = 0;
+
+    StandardToken(GMTAdress).transfer(msg.sender, entitled * 10**decimals);
+  }
+}
